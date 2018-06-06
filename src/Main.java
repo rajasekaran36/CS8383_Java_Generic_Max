b918f1c656308cf75b1313fb9023a96ce8843e7a
Rajasekaran <proffraja@gmail.com>
2018-06-06 15:45:51 +0530

    Manual Added

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		GenericMax max = new GenericMax();
-		Integer[] arr = {14,3,42,5,6,10};
-		String[] hhh = {"R","Ra","Raj"};
-		max.maxFinder(arr);
-		max.maxFinder(hhh);
+		Integer[] numbers = {14,3,42,5,6,10};
+		String[] strings = {"R","Ra","Raj"};
+		max.maxFinder(numbers);
+		max.maxFinder(strings);
 	}
 
 }
